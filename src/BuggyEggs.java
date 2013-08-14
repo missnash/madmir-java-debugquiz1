970cabca815e8f90d0f6f4038cea071dc58ad992
Ashley Nash <msadoylenash@hotmail.com>
2013-08-14 09:29:05 -0400

    Debugging Quiz

--- src/BuggyEggs.java
+++ src/BuggyEggs.java
@@ -5,14 +5,13 @@
  * Look for bad coding practices, as well as logic and syntax errors.
  */
 
-import javax.swing.JOptionPane;
-import java.util.SCan;
+import java.util.Scanner;
 
-public class EggsDemo {
-
+public class BuggyEggs {
+	
 	public static void main(String[] args) {
 		//Declare constants and variables, initialize as appropriate
-		final float PRICE_PER_DOZEN = 3.25;
+		final float PRICE_PER_DOZEN = 3.25f;
 		final float PRICE_PER_SINGLE = 0.45f;
 		int eggs, dozens, singles;
 		float dozensCost, singlesCost, totalCost;
@@ -24,15 +23,15 @@ public class EggsDemo {
 		
 		//This comment is whack, yo!
 		dozens = eggs / 12;
-		singles = eggs * 12;
+		singles = eggs % 12;
 		dozensCost = dozens * PRICE_PER_DOZEN;
 		singlesCost = singles * PRICE_PER_SINGLE;
-		totalCost = PRICE_PER_DOZEN + PRICE_PER_SINGLE;
+		totalCost = dozensCost + singlesCost;
 		
 		//User output
 		System.out.println("You ordered " + eggs + " eggs in total.");
-	System.out.println("That's " + dozens + " dozen at $3.50, or $" + dozensCost);
-		System.out.println("That's" + singles + " singles at 45c, or $" + singlesCost);
+		System.out.println("That's " + dozens + " dozen at $3.25, or $" + dozensCost);
+		System.out.println("That's " + singles + " singles at 45 cents, or $" + singlesCost);
 		System.out.println("Your total cost is $" + totalCost);
 		
 	
